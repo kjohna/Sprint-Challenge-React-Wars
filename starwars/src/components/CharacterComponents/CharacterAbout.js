9d770d01cdb2e28bc840ea847ee4f2608ed46db9
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-11 11:26:59 -0700

    first pass of styling complete, include more character about

--- starwars/src/components/CharacterComponents/CharacterAbout.js
+++ starwars/src/components/CharacterComponents/CharacterAbout.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 function CharacterAbout(props) {
+  const char = props.characterData;
   return (
     <div className="character-about">
-      <h1>{props.characterData.name}</h1>
+      <h1>{char.name}</h1>
       <div className="character-stats">
-        Born: {props.characterData.birth_year}
+        <p>
+          Born: {char.birth_year} <br />
+          Eyes: {char.eye_color}<br />
+          Hair: {char.hair_color}<br />
+          Height: {char.height} cm<br />
+          Weight: {char.mass} kg<br />
+        </p>
       </div>
     </div>
   );
